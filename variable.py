68d7eac072cb46f18893c4aff3403bfacd464cfd
Indrasis Datta <indrasis.datta@yahoo.co.in>
2024-08-29 09:25:32 +0530

    Variable ref & value comparison, random int

--- variable.py
+++ variable.py
@@ -1,7 +1,10 @@
 num1 = 25
 num2 = 25.75
 
-print(type(num1))
-print(type(num2))
+# print(type(num1))
+# print(type(num2))
+# print(float(num1))
 
-print(float(num1))
+import random
+# Random integer value from 0 to 9
+print(random.randint(0, 9))
